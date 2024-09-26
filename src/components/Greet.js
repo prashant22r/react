f1c123082e664b06491109b8b447117a2228bd19
prashant22r <mainiprashant3@gmail.com>
2024-09-26 21:03:57 +0530

    commit

---
+++ src/components/Greet.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './greet.css';
+function Greet(){
+    return <h1 id="g1">this is functional component</h1>
+}
+export default Greet;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css';
+function Header(){
+    return (
+        <header>
+            <h1>my website.</h1>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </header>
+    );
+}
+export default Header;
